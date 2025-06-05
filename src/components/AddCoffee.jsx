df7fe791f5b3af75267397d64e7cd2d1df3c635e
smabari <mdazizulbarishafin@gmail.com>
2025-06-05 11:25:59 +0600

    fix: correct typo in AddCoffee component description

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -33,7 +33,7 @@ const AddCoffee = () => {
       <div className="p-12 text-center space-y-4">
         <h1 className="text-6xl">Add New Coffee</h1>
         <p>
-          It is a long established fact that a reader will be distraceted by the
+          It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. The point of
           using Lorem Ipsum is that it has a more-or-less normal distribution of
           letters, as opposed to using Content here.
